eb2ab0d8274c558fdbd930271567473da9906a18
Andrusha296 <koltsev18@mail.ru>
2024-10-02 11:34:18 +0500

    Add files via upload

---
+++ js.js
@@ -0,0 +1,51 @@
+//document.write('Мороз и солнце; день чудесный!<br>Еще ты дремлешь, друг прелестный<br>Навстречу северной Авроры<br>Звездою севера явись!<br>Пора, красавица, проснись:<br>Открой сомкнуты негой взоры;')
+
+
+
+//Задание 2
+
+
+		// const NAME  = 'Василий';
+		// const LOGIN = 'vasya123'; 
+		// const AGE = '18';
+		// const EMAIL = 'vasya123@mail.ru';
+
+
+		// 	// выведем значения переменных
+		// 	console.log("Исходные значения переменных");
+		// 	console.log('Имя: ' + NAME);
+		// 	console.log('Логин:' + LOGIN);
+		// 	console.log('Возраст:' + AGE);
+		// 	console.log('E-mail:' + EMAIL);
+			
+		// 	// переопределим значения переменных
+		// 	let name = 'Анна';
+		// 	let login = 'anyta987';
+		// 	let age = '17';
+		// 	let email = 'anyta987@mail.ru';
+			
+		// 	// выведем новые значения переменных
+		// 	console.log("Новые значения переменных");
+		// 	console.log('Имя: ' + name);
+		// 	console.log('Логин:' + login);
+		// 	console.log('Возраст:' + age);
+		// 	console.log('E-mail:' + email);
+
+
+//Задание 3
+
+
+// const surname = 'Карамзин  ';
+// const name = 'Виктор';
+// const patronymic = 'Геннадьевич';
+// const ocupation = 'Преподаватель спец. дисциплин';
+// const degree = 'Высшее';
+// const category = 'Первая';
+// const education = 'МГТУ(Тех университет)';
+// const discipline = 'Информатика';
+// const note = 'Шаврардалгормандырктар';
+//  document.write('Должность: ' + ocupation + '<br>');
+//  document.write('Уровень образования:' + degree  + '<br>');
+//  document.write('Категория:' + category  + '<br>');
+//  document.write('Образование:' + education  + '<br>');
+//  document.write('Дисциплина:' + discipline  + '<br>');
